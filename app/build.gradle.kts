acf1ad3047b2e4298512beb42ad1e9be7087065b
muwmx <alekseypered@gmail.com>
2026-09-07 22:25:13 +0300

    chore(app,version): bump version to 1.1.0-Beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
     applicationId = "re.muwmix.yumaplayer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.0-Beta"
+        versionCode = 3
+        versionName = "1.1.0-Beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
